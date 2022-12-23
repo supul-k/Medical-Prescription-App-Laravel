85b9433dde88f3fc0b221163dbdefe218dc8b7ab
Supul Kalhara <supulkalhara7@gmail.com>
2022-12-23 16:55:52 +0530

    Initializing Repository

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    function index(){
+        return view('login');
+    }
+}
